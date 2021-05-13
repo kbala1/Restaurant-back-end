3fbf49fae8c9cbf9480e07f73f49598f867522ee
kiran <kiranrakesh@gmail.com>
2021-05-13 16:39:57 -0500

    Add repository and service

--- src/main/java/com/ga/restaurant/repository/CategoryRepository.java
+++ src/main/java/com/ga/restaurant/repository/CategoryRepository.java
@@ -1,2 +1,12 @@
-package com.ga.restaurant.repository;public interface CategoryRepository {
+package com.ga.restaurant.repository;
+
+
+
+import com.ga.restaurant.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 }
--- src/main/java/com/ga/restaurant/service/CategoryService.java
+++ src/main/java/com/ga/restaurant/service/CategoryService.java
@@ -1,2 +1,23 @@
-package com.ga.restaurant.service;public class CategoryService {
+package com.ga.restaurant.service;
+
+
+import com.ga.restaurant.model.Category;
+import com.ga.restaurant.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public void setCategoryRepository(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+    public List<Category> getCategories() {
+        System.out.println("service calling getCategories ==>");
+        return categoryRepository.findAll();
+    }
 }
