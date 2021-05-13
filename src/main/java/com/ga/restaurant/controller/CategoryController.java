2f3a7a46cc263b7e9c5805aa506669dc70e54fc5
kiran <kiranrakesh@gmail.com>
2021-05-13 16:18:41 -0500

    Add categoryController

--- src/main/java/com/ga/restaurant/controller/CategoryController.java
+++ src/main/java/com/ga/restaurant/controller/CategoryController.java
@@ -1,2 +1,29 @@
-package com.ga.restaurant.controller;public class CategoryController {
+package com.ga.restaurant.controller;
+
+
+import com.ga.restaurant.model.Category;
+import com.ga.restaurant.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api")
+public class CategoryController {
+    private CategoryService categoryService;
+
+    @Autowired
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/categories")
+    public List<Category> getCategories() {
+        System.out.println("calling getCategories ==>");
+        return categoryService.getCategories();
+    }
+
 }
