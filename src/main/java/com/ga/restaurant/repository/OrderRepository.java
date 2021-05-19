47fe24e31fa073b3d6f78ab7ed47b11ad75cfc68
kiran <kiranrakesh@gmail.com>
2021-05-18 22:43:20 -0500

    Throw error if item by id os not found

--- src/main/java/com/ga/restaurant/repository/OrderRepository.java
+++ src/main/java/com/ga/restaurant/repository/OrderRepository.java
@@ -4,9 +4,12 @@ import com.ga.restaurant.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
      Order getOrderById(Long orderId);
      Order getOrderByUserId(Long userId);
+     List<Order> getOrdersByUserId(Long userId);
 
 }
